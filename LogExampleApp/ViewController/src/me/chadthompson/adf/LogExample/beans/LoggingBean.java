d0c31db0a39c0790b121aa203081caeebea1e874
Chad Thompson <chad_thompson@mac.com>
2012-06-18 08:58:24 -0500

    Add Log Example app to "Noodles" project

---
+++ LogExampleApp/ViewController/src/me/chadthompson/adf/LogExample/beans/LoggingBean.java
@@ -0,0 +1,32 @@
+package me.chadthompson.adf.LogExample.beans;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.share.logging.ADFLogger;
+
+public class LoggingBean {
+    
+    private ADFLogger _logger = 
+        ADFLogger.createADFLogger(LoggingBean.class);
+    
+
+    public void logErrorMessage(ActionEvent actionEvent) {
+        // Add event code here...
+        _logger.log(ADFLogger.ERROR, "Error Button Pressed");
+    }
+
+    public void logWarningMessage(ActionEvent actionEvent) {
+        // Add event code here..
+        _logger.log(ADFLogger.WARNING, "Warning Button Pressed");
+    }
+
+    public void logNotificationMessage(ActionEvent actionEvent) {
+        // Add event code here...
+        _logger.log(ADFLogger.NOTIFICATION, "Notification Button Pressed");
+    }
+
+    public void logTraceMessage(ActionEvent actionEvent) {
+        // Add event code here...
+        _logger.log(ADFLogger.TRACE, "Trace Button Pressed");
+    }
+}
